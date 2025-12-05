40429b6b18424210f22e8bf0f29922944cc7b962
LCfreak <warriorlord18@gmail.com>
2025-12-05 11:55:52 +0545

    changing utility

--- utils.py
+++ utils.py
@@ -3,37 +3,36 @@ import os
 import matplotlib
 import torch
 from torch.nn.utils import weight_norm
+import shutil
 matplotlib.use("Agg")
 import matplotlib.pylab as plt
 
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 def plot_spectrogram(spectrogram):
     fig, ax = plt.subplots(figsize=(10, 2))
     im = ax.imshow(spectrogram, aspect="auto", origin="lower",
                    interpolation='none')
     plt.colorbar(im, ax=ax)
-
     fig.canvas.draw()
     plt.close()
-
     return fig
-
 
 def init_weights(m, mean=0.0, std=0.01):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
         m.weight.data.normal_(mean, std)
 
-
 def apply_weight_norm(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
         weight_norm(m)
 
-
 def get_padding(kernel_size, dilation=1):
     return int((kernel_size*dilation - dilation)/2)
-
 
 def load_checkpoint(filepath, device):
     assert os.path.isfile(filepath)
@@ -42,12 +41,10 @@ def load_checkpoint(filepath, device):
     print("Complete.")
     return checkpoint_dict
 
-
 def save_checkpoint(filepath, obj):
     print("Saving checkpoint to {}".format(filepath))
     torch.save(obj, filepath)
     print("Complete.")
-
 
 def scan_checkpoint(cp_dir, prefix):
     pattern = os.path.join(cp_dir, prefix + '????????')
@@ -56,3 +53,9 @@ def scan_checkpoint(cp_dir, prefix):
         return None
     return sorted(cp_list)[-1]
 
+def build_env(config, config_name, path):
+    """Copy config file to checkpoint directory"""
+    t_path = os.path.join(path, config_name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    shutil.copyfile(config, t_path)
